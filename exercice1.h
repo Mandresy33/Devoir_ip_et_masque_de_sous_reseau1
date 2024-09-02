d416a44e67a53841c4cee8d62df405690d54911d
Mandresy33 <167546988+Mandresy33@users.noreply.github.com>
2024-09-02 19:56:09 +0200

    Delete exercice1.h

--- exercice1.h
+++
@@ -1,4 +0,0 @@
-void recupere();
-void fillup(int num1, int num2, int num3, int num4, int *binaire);
-void reseau_bin(int *bin_ip, int *bin_msr, int* bin_net);
-void bin_to_deci(int *bin, int *deci);
