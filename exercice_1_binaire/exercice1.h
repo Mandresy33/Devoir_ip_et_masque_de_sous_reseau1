e389048f741f99b36290b560648b3a9669ce0176
Mandresy33 <167546988+Mandresy33@users.noreply.github.com>
2024-09-02 20:11:45 +0200

    Add files via upload

---
+++ exercice_1_binaire/exercice1.h
@@ -0,0 +1,4 @@
+void recupere();
+void fillup(int num1, int num2, int num3, int num4, int *binaire);
+void reseau_bin(int *bin_ip, int *bin_msr, int* bin_net);
+void bin_to_deci(int *bin, int *deci);
---
+++ exercice_1_binaire/mainExo1.c
@@ -0,0 +1,7 @@
+#include "exercice1.h"
+
+int main()
+{
+	recupere();
+	return 0; 
+}
