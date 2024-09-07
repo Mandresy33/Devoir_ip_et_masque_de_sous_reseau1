f67b303134d677976fdda97902d4198e5da039f2
Mandresy33 <167546988+Mandresy33@users.noreply.github.com>
2024-09-07 11:20:40 +0200

    Update notationcidr.c

--- exo2binaire/notationcidr.c
+++ exo2binaire/notationcidr.c
@@ -10,6 +10,9 @@ void deci_to_bin(int *tab, int *binaire);
 void construct_msr(int nombre_reseau, int *msr_bin);
 void construct_reseau(int *net_bin, int *ip_bin, int *msr_bin);
 void construct_broadcast(int *binaire, int *ipbin, int *bin_broad, int *bin_pass);
+int machine_number(int exposant);
+void bin_to_deci(int *bin, int *deci);
+void ajoute_un(int *broad_deci, int *net_deci);
 void recupere()
 {
     //Variable pour récuperer les données saisies
@@ -30,12 +33,17 @@ void recupere()
     int net_deci[5];
     int net_bin[33];
     int msr_bin[33];
+    int msr_deci[5];
 	//Variable pour le broadcast
 	int broad_bin[33];
+	int broad_deci[5];
 	int bin_pass[33];
 	//Variable pour le découpage
 	int nombre_decoupage;
 	int nombre_sousnet;
+	int new_net;
+	int new_hote; 
+	int nombre_machine = 1; 
     saisie: 
         printf("Entrer l'adresse IP et le nombre de reseau(notation CIDR: adresse/reseau)");
         scanf("%s", donner);
@@ -78,20 +86,29 @@ void recupere()
 							{
 								printf("%d", msr_bin[i]);
 							} 
+							bin_to_deci(msr_bin, msr_deci);
+							printf("\nMasque de sous reseau en base dix: ");
+							printf("%d.%d.%d.%d", msr_deci[0],msr_deci[1],msr_deci[2],msr_deci[3]);
 							//Fabrication de l'adresse reseau
 							construct_reseau(net_bin, ip_bin,msr_bin);
-							printf("\nAdresse reseau en binaire: ");
+							printf("\n\nAdresse reseau en binaire: ");
 							for(i = 31; i>=0; i--)
 							{
 								printf("%d", net_bin[i]);
 							}
+							bin_to_deci(net_bin, net_deci);
+							printf("\nAdresse reseau en base dix: ");
+							printf("%d.%d.%d.%d", net_deci[0],net_deci[1],net_deci[2],net_deci[3]);
 							//Fabrication de l'adresse broadcast
 							construct_broadcast(msr_bin, ip_bin, broad_bin, bin_pass);
-							printf("\nAdresse broadcast en binaire: ");
+							printf("\n\nAdresse broadcast en binaire: ");
 							for(i = 31; i>=0; i--)
 							{
 								printf("%d", broad_bin[i]);
 							}
+							bin_to_deci(broad_bin, broad_deci);
+							printf("\nAdresse de broadcast en base dix: ");
+							printf("%d.%d.%d.%d", broad_deci[0],broad_deci[1],broad_deci[2],broad_deci[3]);
 							//Saisie du nombre de découpage
 							decoupe: 
 								printf("\nEntrer le nombre de découpage\n");
@@ -99,13 +116,45 @@ void recupere()
 								nombre_sousnet = 1;
 								if(nombre_decoupage>=0 && nombre_decoupage <33)
 								{
-									//Nombre de machine
+									//Nombre de sous reseau
 									for(i = 0; i<nombre_decoupage; i++)
 									{
 										nombre_sousnet = nombre_sousnet*2;
 									}
-									printf("Nombre de machine pour chaque sous reseau: %d", nombre_sousnet);
-									//Adresse reseau et broadcast pour chaque sous reseau
+									printf("Nombre de sous reseau apres decoupage: %d", nombre_sousnet);
+									//Nouveau nombre de réseau
+									new_net = nombre_reseau + nombre_decoupage;
+									printf("\nNouveau nombre de reseau pour chaque sous reseau apres decoupage: %d", new_net);
+									//Nouveau nombre de hote
+									new_hote = 32 - new_net;
+									printf("\nNouveau nombre de hote pour chaque sous reseau apres decoupage: %d", new_hote);
+									//Nouveau nombre de machine pour chaquer sous reseau
+									nombre_machine = machine_number(new_hote);
+									printf("\nNombre de machine pour chaque sous reseau: %d\n", nombre_machine);
+									//Adresse reseau et adresse de broadcast pour chaque sous reseau
+									//Pour le premier sous réseau
+									construct_msr(new_net, msr_bin);
+									construct_reseau(net_bin, ip_bin, msr_bin);
+									printf("Pour sous-reseau 1\n");
+									printf("\tAdresse reseau: ");
+									bin_to_deci(net_bin, net_deci);
+									printf("%d.%d.%d.%d", net_deci[0],net_deci[1],net_deci[2],net_deci[3]);
+									construct_broadcast(msr_bin, ip_bin, broad_bin, bin_pass);
+									printf("\n\tAdresse de broadcast:");
+									bin_to_deci(broad_bin, broad_deci);
+									printf("%d.%d.%d.%d", broad_deci[0], broad_deci[1],broad_deci[2],broad_deci[3]);
+									//Pour les autres sous reseau
+									for(i = 0; i<nombre_decoupage-1; i++)
+									{
+										printf("\n\nPour sous-reseau %d", i+2);
+										ajoute_un(broad_deci, net_deci);
+										printf("\n\tAdresse reseau: %d.%d.%d.%d", net_deci[0], net_deci[1],net_deci[2],net_deci[3]);
+										deci_to_bin(net_deci, net_bin);
+										construct_broadcast(msr_bin, net_bin, broad_bin, bin_pass);
+										printf("\n\tAdresse de broadcast:");
+										bin_to_deci(broad_bin, broad_deci);
+										printf("%d.%d.%d.%d\n", broad_deci[0], broad_deci[1],broad_deci[2],broad_deci[3]);
+									}
 								}
 								else
 								{
@@ -131,12 +180,18 @@ void recupere()
 					goto saisie;
 				}
 			}
-        }
-            else
+			else
 			{
-                printf("Erreur!!! Format xxx.xxx.xxx.xxx/nombre reseau\n\n");
-                goto saisie;
+				system("clear");
+				printf("\n\nNombre de reseau manquant!!\n\n");
+				goto saisie;
 			}
+        }
+        else
+		{
+            printf("Erreur!!! Format xxx.xxx.xxx.xxx/nombre reseau\n\n");
+			goto saisie;
+		}
 }
 int slash_verification(char* chaine)
 {
@@ -264,4 +319,80 @@ void construct_broadcast(int *binaire, int *ipbin, int *bin_broad, int *bin_pass
 			bin_broad[i] = 0;
 		}
 	}	
+}
+int machine_number(int exposant)
+{
+	int i = 0;
+	int nombre = 1;
+	for(i=0; i<exposant; i++)
+	{
+		nombre = nombre * 2;
+	}
+	return nombre - 2;
+}
+void bin_to_deci(int *bin, int *deci)
+{
+	int i = 0; 
+	int j = 0;
+	int puissance = 1;
+	deci[2] = 0;
+	deci[3] = 0;
+	deci[1] = 0;
+	deci[0] = 0;
+	for(i = 15; i>=8; i--)
+	{
+		for(j = 8; j<i; j++)
+		{
+			puissance = puissance * 2;
+		}
+		puissance = bin[i] * puissance;
+		deci[2] = deci[2] + puissance;
+		puissance = 1;
+	}
+	for(i = 23; i>=16; i--)
+	{
+		for(j = 16; j<i; j++)
+		{
+			puissance = puissance * 2;
+		}
+		puissance = bin[i] * puissance;
+		deci[1] = deci[1] + puissance;
+		puissance = 1;
+	}
+	for(i = 31; i>=24; i--)
+	{
+		for(j = 24; j<i; j++)
+		{
+			puissance = puissance * 2;
+		}
+		puissance = bin[i] * puissance;
+		deci[0] = deci[0] + puissance;
+		puissance = 1;
+	}
+	for(i=7; i>=0; i--)
+	{
+		for(j = 0; j<i; j++)
+		{
+			puissance = puissance * 2;
+		}
+		puissance = bin[i] * puissance;
+		deci[3] = deci[3] + puissance;
+		puissance = 1;
+	}
+}
+void ajoute_un(int *broad_deci, int *net_deci)
+{
+	for(int j = 0; j<4; j++)
+	{
+		net_deci[j] = broad_deci[j];
+	}
+	net_deci[3]++;
+	for(int j = 3; j>0; j--)
+	{
+		if(net_deci[j] > 255)
+		{
+			net_deci[j] = 0;
+			net_deci[j-1]++;
+		}
+	}
 }
